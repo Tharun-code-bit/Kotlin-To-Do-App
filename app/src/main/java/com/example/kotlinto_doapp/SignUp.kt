fde667b8afdf0c5ce56355dc46964b12590587b1
PadmaGnanapriya <padmaisuru@gmail.com>
2020-10-19 15:59:45 +0530

    Finish sign-up UI

--- app/src/main/java/com/example/kotlinto_doapp/SignUp.kt
+++ app/src/main/java/com/example/kotlinto_doapp/SignUp.kt
@@ -1,11 +1,20 @@
 package com.example.kotlinto_doapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+
+        val link2 = findViewById<TextView>(R.id.sign_up_textView3)
+        link2.setOnClickListener {
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+        }
     }
 }
