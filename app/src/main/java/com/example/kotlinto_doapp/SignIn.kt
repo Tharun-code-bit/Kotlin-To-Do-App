ab4432b3d8828bab7dbe72b4c7797246e599a0a3
PadmaGnanapriya <padmaisuru@gmail.com>
2020-10-19 12:49:25 +0530

    Connect to the firebase

--- app/src/main/java/com/example/kotlinto_doapp/SignIn.kt
+++ app/src/main/java/com/example/kotlinto_doapp/SignIn.kt
@@ -4,12 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        auth = Firebase.auth
 
+        val button = findViewById<Button>(R.id.sign_in)
+        button.setOnClickListener{
+            auth.signInWithEmailAndPassword(editTextTextEmailAddress.text.toString(), editTextTextPassword.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = auth.currentUser
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+
+                        // ...
+                    }
+
+                    // ...
+                }
+        }
     }
 }
